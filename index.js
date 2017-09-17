dc8d44d713beba5ce225b13ca08e7a1b4408d2d7
JCS <joshsad2@gmail.com>
2017-09-17 00:11:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector('#nested .target')
 }
+
+function increaseRankBy(n) {
+  const rankList = document.querySelector('.ranked-list')
+
+  for (let i = 0; l = rankList.length; i < l; i++) {
+    let child = rankedList[i].child;
+
+    for (var j = 0; k = child.length; j < k; j++) {
+      child[j].innerHTML = parseInt(children[J].innerHTML) + n;
+    }
+  }
+}

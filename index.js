1ff01e3f100e54f3a3e97f4e0f64b48a303e302d
JCS <joshsad2@gmail.com>
2017-09-17 00:13:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function increaseRankBy(n) {
   const rankList = document.querySelector('.ranked-list')
 
   for (let i = 0, l = rankList.length; i < l; i++) {
-    let child = rankedList[i].child;
+    let child = rankList[i].child;
 
     for (var j = 0, k = child.length; j < k; j++) {
       child[j].innerHTML = parseInt(child[j].innerHTML) + n;
